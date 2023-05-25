086ab31f9f8171d93e034a9f13f0c74d972c7eca
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-05-26 02:22:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const user = document.getElementById('username');
+const pass = document.getElementById('password');
+const check = document.getElementById('checkbox');
+const submit = document.getElementById('submit');
+const log = document.getElementById('existing');
+
+submit.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(check.checked){
+        localStorage.setItem('username', user.value)
+    }
+    alert(`Logged in as ${user.value}`)
+})
+
+if(localStorage.getItem('username')){
+    log.style.display = 'inline-block';
+    log.addEventListener('click', ()=>{
+        alert(`Logged in as ${localStorage.getItem('username')}`);
+    })
+}
